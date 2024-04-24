e8603797c4f7e906e8bb29bc7c784d7757a329ce
H.D.Hien <huynhduyhien1997@gmail.com>
2024-04-24 14:50:53 +0700

    parking price service

--- app/Http/Resources/ParkingResource.php
+++ app/Http/Resources/ParkingResource.php
@@ -14,6 +14,12 @@ class ParkingResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $totalPrice = $this->total_price ?? ParkingPriceService::calculatePrice(
+            $this->zone_id,
+            $this->start_time,
+            $this->stop_time
+        );
+
         return [
             'id' => $this->id,
             'zone' => [
@@ -23,9 +29,9 @@ class ParkingResource extends JsonResource
             'vehicle' => [
                 'plate_number' => $this->vehicle->plate_number
             ],
-            'start_time' => $this->start_time->toDateTimeString(),
-            'stop_time' => $this->stop_time?->toDateTimeString(),
-            'total_price' => $this->total_price,
+            'start_time' => $this->start_time,
+            'stop_time' => $this->stop_time,
+            'total_price' => $totalPrice,
         ];
     }
 }
